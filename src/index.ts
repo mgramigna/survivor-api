3d470a17bb6dc970e3b8d16f9cf771b4ec13dc8c
Matt Gramigna <matthewgramigna@gmail.com>
2023-09-10 19:17:15 -0400

    chore: docs

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,19 @@ import { seasonService } from "./services/season";
 import { match } from "ts-pattern";
 import { wrapApiResponse } from "./util";
 import { tribeService } from "./services/tribes";
+import swagger from "@elysiajs/swagger";
 
 const app = new Elysia()
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "Survivor API",
+          version: "0.0.1",
+        },
+      },
+    }),
+  )
   .group("/castaways", (app) =>
     app
       .get(
@@ -29,10 +40,19 @@ const app = new Elysia()
           return wrapApiResponse(res, ctx.set);
         },
         {
-          query: t.Object({
-            name: t.Optional(t.String()),
-            season: t.Optional(t.Numeric()),
-          }),
+          query: t.Object(
+            {
+              name: t.Optional(t.String()),
+              season: t.Optional(t.Numeric()),
+            },
+            {
+              description: "Search for castaways by name or season",
+            },
+          ),
+          detail: {
+            summary: "Search for castaways",
+            tags: ["castaways"],
+          },
         },
       )
       .get(
@@ -45,9 +65,18 @@ const app = new Elysia()
           return wrapApiResponse(castawayAndSeasons, ctx.set);
         },
         {
-          params: t.Object({
-            id: t.Numeric(),
-          }),
+          params: t.Object(
+            {
+              id: t.Numeric(),
+            },
+            {
+              description: "Read a specific castaway by id",
+            },
+          ),
+          detail: {
+            summary: "Get a specific castaway",
+            tags: ["castaways"],
+          },
         },
       ),
   )
@@ -61,9 +90,18 @@ const app = new Elysia()
           return wrapApiResponse(seasonsRes, ctx.set);
         },
         {
-          query: t.Object({
-            name: t.Optional(t.String()),
-          }),
+          query: t.Object(
+            {
+              name: t.Optional(t.String()),
+            },
+            {
+              description: "Filter by season name",
+            },
+          ),
+          detail: {
+            summary: "Search for seasons",
+            tags: ["seasons"],
+          },
         },
       )
       .get(
@@ -76,9 +114,18 @@ const app = new Elysia()
           return wrapApiResponse(seasonsRes, ctx.set);
         },
         {
-          params: t.Object({
-            number: t.Numeric(),
-          }),
+          params: t.Object(
+            {
+              number: t.Numeric(),
+            },
+            {
+              description: "Season number",
+            },
+          ),
+          detail: {
+            summary: "Find a specific season by number",
+            tags: ["seasons"],
+          },
         },
       ),
   )
@@ -92,9 +139,18 @@ const app = new Elysia()
           return wrapApiResponse(tribesRes, ctx.set);
         },
         {
-          params: t.Object({
-            id: t.Numeric(),
-          }),
+          params: t.Object(
+            {
+              id: t.Numeric(),
+            },
+            {
+              description: "Find specific tribe by id",
+            },
+          ),
+          detail: {
+            summary: "Find a specific tribe by id",
+            tags: ["tribes"],
+          },
         },
       )
       .get(
@@ -105,18 +161,27 @@ const app = new Elysia()
           return wrapApiResponse(tribesRes, ctx.set);
         },
         {
-          query: t.Object({
-            name: t.Optional(t.String()),
-            type: t.Optional(
-              t.Union([
-                t.Literal<Tribe["type"]>("merge"),
-                t.Literal<Tribe["type"]>("starting"),
-                t.Literal<Tribe["type"]>("swap"),
-                t.Literal<Tribe["type"]>("other"),
-              ]),
-            ),
-            season: t.Optional(t.Numeric()),
-          }),
+          query: t.Object(
+            {
+              name: t.Optional(t.String()),
+              type: t.Optional(
+                t.Union([
+                  t.Literal<Tribe["type"]>("merge"),
+                  t.Literal<Tribe["type"]>("starting"),
+                  t.Literal<Tribe["type"]>("swap"),
+                  t.Literal<Tribe["type"]>("other"),
+                ]),
+              ),
+              season: t.Optional(t.Numeric()),
+            },
+            {
+              description: "By name, type or season",
+            },
+          ),
+          detail: {
+            summary: "Search for tribes",
+            tags: ["tribes"],
+          },
         },
       ),
   )
