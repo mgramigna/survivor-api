e2ab65505d288d325a2a157a4a7a0b9d5b9c2bda
Matt Gramigna <matthewgramigna@gmail.com>
2023-09-21 17:51:14 -0400

    chore: switch to neon as db provider

--- bin/migrate.ts
+++ bin/migrate.ts
@@ -1,7 +1,4 @@
-import { drizzle } from "drizzle-orm/vercel-postgres";
-import { migrate } from "drizzle-orm/vercel-postgres/migrator";
-import { sql } from "@vercel/postgres";
-
-const db = drizzle(sql);
+import { migrate } from "drizzle-orm/neon-http/migrator";
+import { db } from "../src/db";
 
 await migrate(db, { migrationsFolder: "drizzle" });
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,7 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
+neonConfig.fetchConnectionCache = true;
+
+const sql = neon(Bun.env.NEON_DATABASE_URL!);
 export const db = drizzle(sql);
